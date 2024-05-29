7f9835b43a1c0e6d6cef27391bd45da62350f6dd
myesn <myesn@foxmail.com>
2024-05-29 16:41:55 +0800

    feat: view components usage

---
+++ RazorPageTests/Pages/Shared/Components/Card/CardViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorPageTests.Pages.Shared.Components.Card
+{
+    public class CardViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(string? title = null, string titleFontColor = "#000")
+        {
+            var defaultCardInfo = new CardInfoModel
+            {
+                Title = title ?? "default",
+                TitleFontColor = titleFontColor
+            };
+
+            // 传递给视图的数据，model 类型为视图页面 @model 使用的类型
+            return Task.FromResult<IViewComponentResult>(View(defaultCardInfo));
+        }
+
+        public class CardInfoModel
+        {
+            public string Title { get; set; }
+            public string TitleFontColor { get; set; }
+        }
+    }
+}
